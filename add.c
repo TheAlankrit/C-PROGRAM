ee467459c0189ddb2d82959073d4618e7d83f561
Alankrit <alannkritsinha3@gmail.com>
2023-03-14 22:01:27 +0530

    Third Commit

--- add.c
+++ add.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int a=4;
-    int b=5;
-    int c=a+b;
-   printf("%d",c);
+  int a=10,b=-1,c=100,r;
+  r=a++ && b++ || c++;
+ printf("%d",r);
+
 
     return 0;
 }
